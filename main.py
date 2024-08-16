4f440c93c50c658293ebe13656f1f902333e69fc
katdev16 <katlegodhlamini2003@gmail.com>
2024-08-17 00:59:04 +0200

    TESTING CLI

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ from FileManger import FileManager
 manager = FileManager()
 
 
+print("enter command:")
+UserInput = input()
+
+
+
+
+input = UserInput.split(" ")
+
+if input[0].lower()=="command":
+    if input[1]=="create":
+        if input[2]=="name":
+            manager.create_folder("name")
+
